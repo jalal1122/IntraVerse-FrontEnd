6d2fd5d3b1994eb22567ada0d131e12c2327cb3e
Muhammad Jalal <jk4350649@gmail.com>
2025-09-10 12:28:26 +0500

    feat: Implement post details page, enhance post and sidebar components, and improve state management for posts

---
+++ src/Components/SidePost.jsx
@@ -0,0 +1,23 @@
+import { Link } from "react-router";
+
+const SidePost = ({ post }) => {
+  return (
+    <div key={post._id} className="flex gap-2 p-2 hover:cursor-pointer">
+      <Link to={`/post/${post._id}`}>
+        <img
+          src={post.image}
+          alt={post.title}
+          className="w-30 h-18 object-cover mr-2 hover:scale-105 transition-transform duration-300 hover:cursor-pointer"
+        />
+      </Link>
+      <div>
+        <Link to={`/post/${post._id}`}>
+          <h4 className="font-semibold hover:underline underline-offset-4">{post.title}</h4>
+        </Link>
+        <p className="text-sm text-gray-600">{post.createdAt.split("T")[0]}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SidePost;
