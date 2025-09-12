647f514bfa22bc3e8bd0fa34ed2693f96a1a9f80
muhammad uzair <abuzada735@gmail.com>
2025-09-13 00:32:17 +0500

    feat: Configure axios for environment-based API URL and add production environment variables

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+const localLink = "http://localhost:5000";
+const productionLink = import.meta.env.VITE_APP_API_URL;
+
 // Set up default configuration for axios
-axios.defaults.baseURL = "http://localhost:5000"; // Adjust to match your backend port
+axios.defaults.baseURL =
+  import.meta.env.VITE_APP_NODE_ENV === "production"
+    ? productionLink
+    : localLink;
 axios.defaults.withCredentials = true;
 
 export default axios;
