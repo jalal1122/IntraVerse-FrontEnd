70d4f0550706502c90a6c7014a5f30d114c8462c
muhammad uzair <abuzada735@gmail.com>
2025-09-20 15:56:22 +0500

    feat: add Adsterra ad components to PostDetails for enhanced monetization

--- src/Pages/PostDetails.jsx
+++ src/Pages/PostDetails.jsx
@@ -5,6 +5,8 @@ import Post from "../Components/Post Details_Components/Post";
 import SideBar from "../Components/DashBoard_Components/SideBar";
 import Comments from "../Components/Post Details_Components/Comments";
 import MakeComments from "../Components/Post Details_Components/MakeComments";
+import AdsterraAd from "../Components/AdsterraAd";
+import AdsterraPopunder from "../Components/AdsterraPopunder";
 
 const PostDetails = () => {
   // get the id from the params
@@ -15,7 +17,6 @@ const PostDetails = () => {
 
   return (
     <div>
-
       <Header />
 
       <div
@@ -33,6 +34,8 @@ const PostDetails = () => {
         {/* Side Bar */}
         <div className="w-full lg:w-[29%] h-[100%] rounded-lg flex flex-row lg:flex-col flex-wrap justify-center gap-10 p-5">
           <SideBar start={0} end={2} title={"Trending"} />
+          <AdsterraAd />
+          <AdsterraPopunder />
           <SideBar start={2} end={4} title={"Popular"} />
 
           <Comments />
